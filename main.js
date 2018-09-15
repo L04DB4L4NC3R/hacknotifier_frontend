b5206309a8ef0bfdda25751ca127c5f616c75266
angad <angadsharma1016@gmail.com>
2018-09-16 00:43:51 +0530

    electron init

---
+++ main.js
@@ -0,0 +1,42 @@
+const {
+    app,
+    BrowserWindow
+} = require("electron");
+
+const path = require("path");
+const url = require("url");
+
+let win;
+
+let createWindow = ()=>{
+
+    win1 = new BrowserWindow({show:false});
+
+    win1.loadURL(url.format({
+        pathname:path.join(__dirname,"views/index.html"),
+        protocol:"file",
+        slashes:true
+    }));
+
+    win1.on("ready-to-show",()=>{
+        win1.maximize();
+        win1.show();
+    });
+
+    win1.on("closed",()=>{
+        win1=null;
+    });
+
+}
+
+
+app.on("ready",createWindow);
+
+app.on("window-all-closed",()=>{
+    if(process.platform !== "darwin")
+        app.quit();
+});
+
+app.on("activate",()=>{
+    createWindow();
+});
